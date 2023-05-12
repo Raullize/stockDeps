387fa0b2b781613b29b17f813a01a51871bed778
unknown <elano.tavares.ncs@gmail.com>
2023-05-12 16:31:42 -0300

    bd completo

--- 01-bd/stock-deps.sql
+++ 01-bd/stock-deps.sql
@@ -77,7 +77,6 @@ LOCK TABLES `produtos` WRITE;
 /*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
 UNLOCK TABLES;
 
-
 DROP TABLE IF EXISTS `clientes`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -103,4 +102,61 @@ CREATE TABLE `clientes` (
 LOCK TABLES `clientes` WRITE;
 /*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
 /*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `entradas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `entradas` (
+                             `id` int(11) NOT NULL AUTO_INCREMENT,
+                             `idCategoria` int(11) NOT NULL,
+                             `idProdutos` int(11) NOT NULL,
+                             `quantidade` int(11) NOT NULL,
+                             `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+                             `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+                             PRIMARY KEY (`id`),
+                             KEY `fk_entradas_categoria_idx` (`idCategoria`),
+                             CONSTRAINT `fk_entradas_categoria` FOREIGN KEY (`idCategoria`) REFERENCES `categoria` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+                             KEY `fk_entradas_produtos_idx` (`idProdutos`),
+                             CONSTRAINT `fk_entradas_produtos` FOREIGN KEY (`idProdutos`) REFERENCES `produtos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `entradas`
+--
+
+LOCK TABLES `entradas` WRITE;
+/*!40000 ALTER TABLE `entradas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `entradas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `saidas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `saidas` (
+                             `id` int(11) NOT NULL AUTO_INCREMENT,
+                             `idCategoria` int(11) NOT NULL,
+                             `idClientes` int(11) NOT NULL,
+                             `idProdutos` int(11) NOT NULL,
+                             `quantidade` int(11) NOT NULL,
+                             `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+                             `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp(),
+                             PRIMARY KEY (`id`),
+                             KEY `fk_saidas_categoria_idx` (`idCategoria`),
+                             CONSTRAINT `fk_saidas_categoria` FOREIGN KEY (`idCategoria`) REFERENCES `categoria` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+                             KEY `fk_saidas_clientes_idx` (`idClientes`),
+                             CONSTRAINT `fk_saidas_clientes` FOREIGN KEY (`idClientes`) REFERENCES `clientes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+                             KEY `fk_saidas_produtos_idx` (`idProdutos`),
+                             CONSTRAINT `fk_saidas_produtos` FOREIGN KEY (`idProdutos`) REFERENCES `produtos` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `saidas`
+--
+
+LOCK TABLES `saidas` WRITE;
+/*!40000 ALTER TABLE `saidas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `saidas` ENABLE KEYS */;
 UNLOCK TABLES;
