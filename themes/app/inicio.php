5d9c15c7e6747e8997cc2ecc59909a236549238c
Kaio Leote Silveira <kaiolsilveira5@gmail.com>
2023-08-10 21:28:26 -0300

    baixa aqui miguel

--- themes/app/inicio.php
+++ themes/app/inicio.php
@@ -53,54 +53,54 @@ echo '</script>';
   </div>
 
 
-  <div class="relatorio">
-    <div class="container">
-      <div class="card">
-        <div class="cartao-pequeno">
-          <p class="titulo-relatorio">Vendas</p>
-          <p class="resposta" id="qtd-vendas"></p>
-        </div>
+
+  <div class="container1">
+    <div class="card">
+      <div class="cartao-pequeno">
+        <p class="titulo-relatorio">Vendas</p>
+        <p class="resposta" id="qtd-vendas"></p>
       </div>
+    </div>
 
-      <div class="card">
-        <div class="cartao-medio">
-          <p class="titulo-relatorio">Mais vendidos:</p>
-          <p class="resposta-letra" id="ranking-categorias"></p>
+    <div class="card">
+      <div class="cartao-medio">
+        <p class="titulo-relatorio">Mais vendidos:</p>
+        <p class="resposta-letra" id="ranking-categorias"></p>
 
-        </div>
       </div>
+    </div>
 
-      <div class="card">
-        <div class="cartao-pequeno">
-          <p class="titulo-relatorio">Renda</p>
-          <p class="resposta">R$ 20000 </p>
-        </div>
+    <div class="card">
+      <div class="cartao-pequeno">
+        <p class="titulo-relatorio">Renda</p>
+        <p class="resposta">R$ 20000 </p>
       </div>
+    </div>
 
-      <div class="card">
-        <div class="cartao-medio">
-          <p class="titulo-relatorio">Clientes</p>
-          <p class="resposta" id="qtd-clientes"></p>
-        </div>
+    <div class="card">
+      <div class="cartao-medio">
+        <p class="titulo-relatorio">Clientes</p>
+        <p class="resposta" id="qtd-clientes"></p>
       </div>
-
-
-      <div class="card">
-        <div class="cartao-grande" id="conta-itens">
-          <p class="titulo-relatorio mb-4">Itens em cada categoria:</p>
-          <p class="resposta-letra" id="lava-roupas-qtd">Lava roupas = 0</p>
-          <p class="resposta-letra" id="lava-loucas-qtd">Lava louças = 0</p>
-          <p class="resposta-letra" id="lava-carros-qtd">Lava carro = 0</p>
-          <p class="resposta-letra" id="limpeza-ambiente-qtd">Limpeza de Ambiente = 0</p>
-          <p class="resposta-letra" id="outros-qtd">Outros = 0</p>
-          <p class="resposta mt-3" id="total-produtos-qtd">Total = 0</p>
-        </div>
-      </div>
-
     </div>
 
 
+
+
+    <div class="card">
+      <div class="cartao-grande" id="conta-itens">
+        <p class="titulo-relatorio mb-4">Itens em cada categoria:</p>
+        <p class="resposta-letra" id="lava-roupas-qtd">Lava roupas = 0</p>
+        <p class="resposta-letra" id="lava-loucas-qtd">Lava louças = 0</p>
+        <p class="resposta-letra" id="lava-carros-qtd">Lava carro = 0</p>
+        <p class="resposta-letra" id="limpeza-ambiente-qtd">Limpeza de Ambiente = 0</p>
+        <p class="resposta-letra" id="outros-qtd">Outros = 0</p>
+        <p class="resposta mt-3" id="total-produtos-qtd">Total = 0</p>
+      </div>
+    </div>
   </div>
+
+
 
   <script src="<?= url('assets/app/js/home.js') ?>"></script>
   <script src="<?= url('assets/app/js/procurarClientes.js') ?>"></script>
