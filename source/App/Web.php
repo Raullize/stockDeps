52701557e242fab1b8d084f00ff3c182d847b931
Raul Lize Teixeira <elano.tavares.ncs@gmail.com>
2024-12-23 21:23:41 -0300

    teste

--- source/App/Web.php
+++ source/App/Web.php
@@ -33,6 +33,8 @@ class Web
             return;
         }
 
+        // teste
+
         $session = new Session();
         $user = new Users();
         $userDados = $user->selectUserByName($data["username"]);
